b3a00e818a7ac8bb08a0738699426f5d20ad3b76
Andre Sobral <andre.g.sobral@gmail.com>
2015-10-23 18:50:12 +0100

    ficheiros antigos que nao tem nd a ver com o projeto de IA.

--- max.lisp
+++
@@ -1,6 +0,0 @@
-;Funcao maximo de dois numeros
-(defun max_test (x y)
-  (if (> x y)
-      x
-    y))
-
--- max3.lisp
+++
@@ -1,8 +0,0 @@
-;Funcao maximo de 3 numeros
-(defun max3 (x y z)
-  (if (> x y z)
-      x
-    (if (> y z x)
-        y
-      z)))
-
