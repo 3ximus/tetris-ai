85f1b548aefe24772b90455e895a5cf2ed3cfb8e
Andre Sobral <andre.g.sobral@gmail.com>
2015-10-23 19:14:34 +0100

    setup inicial

--- proj1/tetris.lisp
+++ proj1/tetris.lisp
@@ -1,3 +1,13 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;										 			 		;
+;				Projeto IA 2015/2016 --- grupo 59	 		;
+;					Andre Sobral   nº 69481			 		;
+;					Rui Lourenco   nº 69701			 		;
+;					Fabio Almeida  nº 76959			 		;
+;										 			 		;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; Tipo Accao ;;;
 (defun cria-accao (posicao peca)
   (cons posicao peca))
 
@@ -7,6 +17,7 @@
 (defun accao-peca (accao)
   (rest accao))
 
+;;; Tipo Tabuleiro ;;;
 (defstruct tabuleiro (linhas 18) (colunas 10))
 
 (defun cria-tabuleiro ()
