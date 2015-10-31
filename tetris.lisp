df336bf2d95a45d9b36f17fdecb1455131a8d5bd
AndreSobral <andre.g.sobral@gmail.com>
2015-10-31 18:27:29 +0000

    tabuleiro->array a funcionar

--- tetris.lisp
+++ tetris.lisp
@@ -97,7 +97,7 @@
 ;;; Cria um array com o conteudo do tabuleiro
 ;;;
 (defun tabuleiro->array (tabuleiro)
-  (tabuleiro-data tabuleiro))
+  (copy-array (tabuleiro-data tabuleiro)))
 
 ;;;
 ;;; Cria um tabuleiro com conteudo igual a (arrayOfContent)
@@ -117,3 +117,20 @@
   (if (or(> coluna (- *COLUNAS* 1))(< coluna 0))
       NIL
     T))
+
+(defun copy-array (array &key
+                         (element-type (array-element-type array))
+                         (fill-pointer (and (array-has-fill-pointer-p array)(fill-pointer array)))
+                         (adjustable (adjustable-array-p array)))
+  "Returns an undisplaced copy of ARRAY, with same fill-pointer and
+adjustability (if any) as the original, unless overridden by the keyword
+arguments."
+  (let* ((dimensions (array-dimensions array))
+         (new-array (make-array dimensions
+                                :element-type element-type
+                                :adjustable adjustable
+                                :fill-pointer fill-pointer)))
+    (dotimes (i (array-total-size array))
+      (setf (row-major-aref new-array i)
+            (row-major-aref array i)))
+    new-array))
