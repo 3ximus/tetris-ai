e57848dc07af18d8819763474bc744adad677563
eximus <fabio_r11@hotmail.com>
2015-11-01 17:15:59 +0000

    simplificar o copy-array

--- tetris.lisp
+++ tetris.lisp
@@ -100,7 +100,7 @@
 ;;; Cria um array com o conteudo do tabuleiro
 ;;;
 (defun tabuleiro->array (tabuleiro)
-  (copy-array (tabuleiro-data tabuleiro)))
+  (copia-array (tabuleiro-data tabuleiro)))
 
 ;;;
 ;;; Cria um tabuleiro com conteudo igual a (arrayOfContent)
@@ -125,18 +125,9 @@
 ;;;
 ;;; Faz a copia de um array
 ;;;
-(defun copy-array (array &key
-                         (element-type (array-element-type array))
-                         (fill-pointer (and (array-has-fill-pointer-p array)(fill-pointer array)))
-                         (adjustable (adjustable-array-p array)))
-  "Returns an undisplaced copy of ARRAY, with same fill-pointer and
-adjustability (if any) as the original, unless overridden by the keyword
-arguments."
+(defun copia-array (array)
   (let* ((dimensions (array-dimensions array))
          (new-array (make-array dimensions
-                                :element-type element-type
-                                :adjustable adjustable
-                                :fill-pointer fill-pointer)))
     (dotimes (i (array-total-size array))
       (setf (row-major-aref new-array i)
             (row-major-aref array i)))
