45d367696e1f40a507e6df2801ec5add6073b33f
AndreSobral <andre.g.sobral@gmail.com>
2015-11-27 15:25:32 +0000

    aggregate height - heuristica 1

--- tetris.lisp
+++ tetris.lisp
@@ -433,37 +433,31 @@
 ;;;                  a partir desse estado ate ao melhor estado objectivo
 ;;; Devolve uma sequencia de accoes em que (do inicio para o fim) representam uma solucao do problema 
 ;;; -----------------------------------------------------------
-(defun procura-A* (problema heuristica)
-  (let* ((estado (problema-estado-inicial problema))
-	(lista NIL)
-	(infinity 99999999)
-	(menor-custo infinity)
-	(accao-a-escolher)
-	(estado-a-escolher NIL))
-	(loop 
-	  ;;; iterar sobre todos os possiveis estados
-	  (dolist (accao (funcall (problema-accoes problema) estado))
-	    ;;; calcula estdo e custos
-	    (let* ((estado-resultado (funcall (problema-resultado problema) estado accao))
-		   (custo (funcall (problema-custo-caminho problema) estado-resultado))
-		   (custo-total (+ custo (funcall heuristica estado-resultado))))
-			(format T "~D/~D     ~S      ~S~%" custo (estado-pontos estado-resultado) (estado-pecas-colocadas estado-resultado)  accao)
-		   ;;; se custo calculado for minimo escolhemos esse como melhor estado
-		   (if (<= custo-total menor-custo) (progn (setf menor-custo custo) (setf estado-a-escolher estado-resultado)(setf accao-a-escolher accao)))))
-	  ;;; adiciona melhor estado a lista e prepara proxima iteracao caso a heuristica deste estado nao seja 0, se for retorna da funcao
-(format T "------------------------------------~%" (estado-pecas-por-colocar estado-a-escolher))
-	  (setf lista (append lista (list accao-a-escolher)))
-	  (when (and (= (funcall heuristica estado-a-escolher) 0) (funcall (problema-solucao problema) estado-a-escolher))(return lista))
-	  (setf menor-custo infinity)
-	  (setf estado estado-a-escolher))))
+;;(defun procura-A* (problema heuristica))
 
 ;;; -----------------------------------------------------------
 ;;; Identifica a melhor procura possivel  
-;;;  - tabuleiro 
+;;;  - array com as posicoes do tabuleiro 
 ;;;  - lista-pecas -> lista de pecas por colocar 
 ;;; Devolve uma sequencia de accoes em que (do inicio para o fim) representam uma solucao do problema 
 ;;; -----------------------------------------------------------
 
-;;;(defun procura-best (tabuleiro lista-pecas))
+(defun procura-best (array-tab lista-pecas))
 
-(load "utils.fas")
+
+;;; Funcoes heuristicas
+
+;;; Soma as alturas de um tabuleiro
+;;; - tabuleiro
+;;; Devolve a soma das alturas de cada coluna do tabuleiro
+;;; Queremos minimizar este valor
+(defun soma-alturas-tabuleiro (tabuleiro)
+  (let ((soma-alturas 0))
+  (dotimes (coluna *COLUNAS* soma-alturas)
+    (format T "coluna [~D]: ~D~%" (+ coluna 1) (tabuleiro-altura-coluna tabuleiro coluna))
+    (setf soma-alturas (+ soma-alturas (tabuleiro-altura-coluna tabuleiro coluna))))))
+
+
+
+
+;;(load "utils.fas")
