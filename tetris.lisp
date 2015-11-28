d86002797c74ff0aab45bfe7c18ba4bd0504f294
AndreSobral <andre.g.sobral@gmail.com>
2015-11-28 00:33:10 +0000

    falta lista de estados aberto. Nao passa aos testes 17, 18 e 21

--- tetris.lisp
+++ tetris.lisp
@@ -425,25 +425,27 @@
 ;;; -----------------------------------------------------------
 (defun procura-A* (problema heuristica)
   (let* ((estado (problema-estado-inicial problema))
-	(lista NIL)
-	(infinity 99999999)
-	(menor-custo infinity)
-	(accao-a-escolher)
-	(estado-a-escolher NIL))
-	(loop 
+	       (lista NIL)
+	       (infinity 99999999)
+	       (menor-custo infinity)
+	       (accao-a-escolher NIL)
+	       (estado-a-escolher estado))
+	(loop
+    (when (funcall (problema-solucao problema) estado-a-escolher) (return lista)) 
 	  ;;; iterar sobre todos os possiveis estados
 	  (dolist (accao (funcall (problema-accoes problema) estado))
 	    ;;; calcula estdo e custos
 	    (let* ((estado-resultado (funcall (problema-resultado problema) estado accao))
-		   (custo (funcall (problema-custo-caminho problema) estado-resultado))
-		   (custo-total (+ custo (funcall heuristica estado-resultado))))
-			(format T "~D/~D     ~S      ~S~%" custo (estado-pontos estado-resultado) (estado-pecas-colocadas estado-resultado)  accao)
-		   ;;; se custo calculado for minimo escolhemos esse como melhor estado
-		   (if (<= custo-total menor-custo) (progn (setf menor-custo custo) (setf estado-a-escolher estado-resultado)(setf accao-a-escolher accao)))))
-	  ;;; adiciona melhor estado a lista e prepara proxima iteracao caso a heuristica deste estado nao seja 0, se for retorna da funcao
-(format T "------------------------------------~%" (estado-pecas-por-colocar estado-a-escolher))
-	  (setf lista (append lista (list accao-a-escolher)))
-	  (when (and (= (funcall heuristica estado-a-escolher) 0) (funcall (problema-solucao problema) estado-a-escolher))(return lista))
+		         (custo (+ (funcall (problema-custo-caminho problema) estado-resultado) (funcall heuristica estado-resultado))))
+      ;(format T "custo: ~D || accao: ~S~%" custo accao)
+		  ;;; se custo calculado for minimo escolhemos esse como melhor estado
+		  (if (<= custo menor-custo) 
+        (progn 
+        (setf menor-custo custo) 
+        (setf estado-a-escolher estado-resultado)
+        (push accao accao-a-escolher)))))
+	  ;;; adiciona melhor accao a lista e prepara proxima iteracao
+	  (setf lista (append lista (list (first accao-a-escolher))))
 	  (setf menor-custo infinity)
 	  (setf estado estado-a-escolher))))
 
