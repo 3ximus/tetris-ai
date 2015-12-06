bbcfcc43419bee82f508ca7e56e354f77c0db2b3
AndreSobral <andre.g.sobral@gmail.com>
2015-12-06 16:10:00 +0000

    25 testes

--- procura-best-Tests.sh
+++ procura-best-Tests.sh
@@ -3,33 +3,26 @@
 echo "Starting tests."
 
 echo "Running test01..........."
-clisp -i tetris.lisp -i utils.lisp < test01/input > test01/stats.txt
+clisp -i tetris.lisp -i utils.lisp < tests/test01/input > tests/test01/stats.txt
 echo "Running test02..........."
-clisp -i tetris.lisp -i utils.lisp < test02/input > test02/stats.txt
-echo "Running test03........... [ 90 pecas ]"
-clisp -i tetris.lisp -i utils.lisp < test03/input > test03/stats.txt
-echo "Running test24..........."
-clisp -i tetris.lisp -i utils.lisp < test24/input > test24/stats.txt
-echo "Running test25..........."
-clisp -i tetris.lisp -i utils.lisp < test25/input > test25/stats.txt
+clisp -i tetris.lisp -i utils.lisp < tests/test02/input > tests/test02/stats.txt
+echo "Running test03..........."
+clisp -i tetris.lisp -i utils.lisp < tests/test03/input > tests/test03/stats.txt
+
 
 echo "diff test01 outputs"
-diff test01/output test01/output.txt
+diff tests/test01/output tests/test01/output.txt
 echo "diff test02 outputs"
-diff test02/output test02/output.txt
+diff tests/test02/output tests/test02/output.txt
 echo "diff test03 outputs"
-diff test03/output test03/output.txt
-echo "diff test24 outputs"
-diff test24/output test24/output.txt
-echo "diff test25 outputs"
-diff test25/output test25/output.txt
+diff tests/test03/output tests/test03/output.txt
+
 
 echo "Filtering stats.........."
-tail -n +23 test01/stats.txt > test01/FilteredStats.txt # Test01
-tail -n +23 test02/stats.txt > test02/FilteredStats.txt # Test02
-tail -n +23 test03/stats.txt > test03/FilteredStats.txt # Test03
-tail -n +23 test24/stats.txt > test24/FilteredStats.txt # Test24
-tail -n +23 test25/stats.txt > test25/FilteredStats.txt # Test25
+tail -n +23 tests/test01/stats.txt > tests/test01/FilteredStats.txt # Test01
+tail -n +23 tests/test02/stats.txt > tests/test02/FilteredStats.txt # Test02
+tail -n +23 tests/test03/stats.txt > tests/test03/FilteredStats.txt # Test03
+
 
 # Apaga ficheiros stats.txt de cada teste.
 # Ficheiro stat.txt tem o output produzido pelo clisp
